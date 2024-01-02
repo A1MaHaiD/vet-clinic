9f373e3fe0d15b1f5cf994173c9d8816036e1446
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-01-02 13:16:55 +0200

    REQ: 06-02 add sort Pet by age (PetAgeComparator.java) into Sandbox.java

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,7 +1,10 @@
 package main.java.com.magicvet;
 
 import main.java.com.magicvet.comparator.DogSizeComparator;
+import main.java.com.magicvet.comparator.PetAgeComparator;
+import main.java.com.magicvet.model.Cat;
 import main.java.com.magicvet.model.Dog;
+import main.java.com.magicvet.model.Pet;
 
 import java.util.Arrays;
 
@@ -16,10 +19,25 @@ public class Sandbox {
                 new Dog(Dog.XS),
                 new Dog(Dog.S),
         };
+        Pet[] pets = {
+                new Cat("cat", "female", "24", "Світлана", "Олександр Лук'янчук"),
+                new Dog("dog", "female", "24", "Яна", "Олександр Лук'янчук", Dog.S),
+                new Cat("cat","female","26","Валерія","Олександр Лук'янчук"),
+                new Cat("cat","female","4","Ракета","Олександр Лук'янчук"),
+                new Cat("cat","female","2","Світлана","Олександр Лук'янчук"),
+                new Cat("cat","female","2","Світлана","Олександр Лук'янчук"),
+                new Cat("cat","male","3","Шашлик","Олександр Лук'янчук"),
+                new Cat("cat","female","3","Малібель","Олександр Лук'янчук"),
+        };
 
         Arrays.sort(dogs, new DogSizeComparator());
         for (Dog dog : dogs) {
             System.out.println(dog.getSize());
+        }
+
+        Arrays.sort(pets, new PetAgeComparator());
+        for (Pet pet:pets){
+            System.out.println(pet.getAge());
         }
     }
 }
---
+++ src/main/java/com/magicvet/comparator/PetNameComparator.java
@@ -0,0 +1,12 @@
+package main.java.com.magicvet.comparator;
+
+import main.java.com.magicvet.model.Pet;
+
+import java.util.Comparator;
+
+public class PetNameComparator implements Comparator<Pet> {
+    @Override
+    public int compare(Pet o1, Pet o2) {
+        return 0;
+    }
+}
